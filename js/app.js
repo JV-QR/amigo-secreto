501bbfc152fcdb415d673be466073975211c1ff3
João Victor Ribeiro <victor.19ribeiro@gmail.com>
2024-12-13 11:40:24 -0300

    Embaralhando a lista de amigos; Ativando botão reiniciar

--- js/app.js
+++ js/app.js
@@ -1,11 +1,54 @@
+let listaAmigos = [];
+
 function adicionar(id) {
-    
+    //recuperar o valor nome do amigo
+    let amigo = document.getElementById('nome-amigo').value;
+
+    //adicionar ele na lista de amigos incluidos.
+    let lista = document.getElementById('lista-amigos');
+
+    listaAmigos.push(amigo);
+
+    if(lista.textContent == '') {
+        lista.textContent = amigo;
+    } else {
+        lista.textContent = lista.textContent + ', ' + amigo;
+    }
+
+    document.getElementById('nome-amigo').value = '';
 }
 
-function sortear() {
 
+function sortear() {
+    embaralha(listaAmigos);
+    let sorteio = document.getElementById('lista-sorteio');
+
+    for (let i = 0; i < listaAmigos.length; i++) {
+
+        if(i == listaAmigos.length - 1) {
+            sorteio.innerHTML = sorteio.innerHTML + listaAmigos[i] + '-->' + listaAmigos[0] + '<br>';
+        } else {
+            sorteio.innerHTML = sorteio.innerHTML + listaAmigos[i] + '-->' + listaAmigos[i + 1] + '<br>';
+        }
+    }
+}
+
+
+function embaralha(lista) {
+
+    for (let indice = lista.length; indice; indice--) {
+
+        const indiceAleatorio = Math.floor(Math.random() * indice);
+
+        // atribuição via destructuring
+        [lista[indice - 1], lista[indiceAleatorio]] = 
+            [lista[indiceAleatorio], lista[indice - 1]];
+    }
 }
 
 function reiniciar() {
-
+    listaAmigos = [];
+    document.getElementById('lista-amigos').innerHTML = '';
+    document.getElementById('lista-sorteio').innerHTML = '';
+    // alert('botão funcionando');
 }
